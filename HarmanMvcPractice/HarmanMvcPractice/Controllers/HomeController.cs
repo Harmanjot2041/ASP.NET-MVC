03a6a18a1ff4dc28e8c74fa1c55f3124ffd9a223
Harmanjot2041 <71279336+Harmanjot2041@users.noreply.github.com>
2021-02-25 20:42:52 +0530

    Basics mvc app with actions ,routing , model, controllers etc

---
+++ HarmanMvcPractice/HarmanMvcPractice/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HarmanMvcPractice.Models;
+
+namespace HarmanMvcPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        List<SignUp> employeeList;
+       
+       
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            employeeList = new List<SignUp>();
+            employeeList.Add(new SignUp
+            {
+                Id = 1,
+                Name = "Harmanjot",
+                Age = 21,
+                Desination = "UIET",
+                Salary = 300000
+            });
+            employeeList.Add(new SignUp
+            {
+                Id = 2,
+                Name = "Gunika",
+                Age = 21,
+                Desination = "UIET",
+                Salary = 1000000
+            });
+            employeeList.Add(new SignUp
+            {
+                Id = 3,
+                Name = "Venketesh",
+                Age = 21,
+                Desination = "PEC",
+                Salary = 3000000
+            });
+            employeeList.Add(new SignUp
+            {
+                Id = 4,
+                Name = "Abhishek",
+                Age = 21,
+                Desination = "CU",
+                Salary = 600000
+            });
+
+        }
+
+        public IActionResult Index()
+        {
+            return View(employeeList);
+           // return Content("Heyyyyyyyyyyyyyyyyyyyyy");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult EmployeeView()
+        {
+            return View(employeeList);
+        }
+        public IActionResult Content(int id)
+        {
+            return Content("Hello from Content " + id);
+        }
+        public JsonResult Json ()
+        {
+            return Json(employeeList);
+        }
+        [HttpGet]
+        public IActionResult EditEmployee(long id )
+        {
+            var model = new SignUp();
+            if (id > 0)
+                model = employeeList.FirstOrDefault(x => x.Id == id);
+          
+            return View("_EditEmpployeePartial", model);
+           // return PartialView("_EditEmpployeePartial", employeeList.FirstOrDefault(x=> x.Id == id));
+        }
+        [HttpPost]
+        public IActionResult EditEmployee(SignUp model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(EmployeeView));
+            }
+            else
+                return PartialView("_EditEmpployeePartial", model);
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HarmanMvcPractice/HarmanMvcPractice/Models/SignUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace HarmanMvcPractice.Models
+{
+    public class SignUp
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range (0,40, ErrorMessage ="Age should be less than 40 ")]
+        public int Age { get; set; }
+        [Required]
+        [Range(0, 4000000, ErrorMessage = "Age should be less than 40 ")]
+        public int Salary { get; set; }
+        [Required]
+        public string Desination { get; set; }
+        
+    }
+}
